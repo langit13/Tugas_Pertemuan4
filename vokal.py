95cccda4504694035f3013c077b6a034ef15196a
Muhammad Azmi Nur Iman <113041333+langit13@users.noreply.github.com>
2022-09-25 05:24:31 +0700

    Update vokal.py

--- vokal.py
+++ vokal.py
@@ -1,27 +1,8 @@
 # Delete huruf vokal
-#def hapusVokal(kalimat):
-    #for i in "aeiouAEIOU":
+def hapusVokal(kalimat):
+    for i in "aeiouAEIOU":
         # Menggunakan fungsi replace untuk merubah setiap huruf vokal menjadi string kosong
-        #kalimat = kalimat.replace(i, "")
-    #return kalimat
+        kalimat = kalimat.replace(i, "")
+    return kalimat
 
-#print(hapusVokal(input("")))#
-
-
-
-#x = input("")
-
-#vocal = ["a", "i", "u", "e", "o"]
-#result = ""
-
-#for i in x.lower():
-    #if i not in vocal:
-        #result += i
-
-#print("result : ", result)
-
-
-
-x = input("").replace("a","").replace("i","").replace("u","").replace("e","").replace("o","").lower()
-
-print(x)
+print(hapusVokal(input("")))
